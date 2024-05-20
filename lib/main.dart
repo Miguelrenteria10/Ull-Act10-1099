9c6c6df08ef05f4c4053c74c5760542541f4d335
MIGUEL_JARED_RENTERIA_MARRUFO <a.21308051281099@cbtis128.edu.mx>
2024-05-20 15:23:47 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+import 'package:act10/formulario1.dart';
+import 'package:act10/formulario2.dart';
+import 'package:act10/formulario3.dart';
+import 'package:act10/formulario4.dart';
+import 'package:act10/formulario5.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Sabritas',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  String _getTitle(int index) {
+    switch (index) {
+      case 0:
+        return 'Cliente';
+      case 1:
+        return 'Empleados';
+      case 2:
+        return 'Productos';
+      case 3:
+        return 'Provedores';
+      case 4:
+        return 'Ventas';
+      default:
+        return 'Iniciar Sesion';
+    }
+  }
+
+  Widget _getPageBody(int index) {
+    switch (index) {
+      case 0:
+        return PageOne();
+      case 1:
+        return PageTwo();
+      case 2:
+        return PageThree();
+      case 3:
+        return PageFour();
+      case 4:
+        return PageFive();
+      default:
+        return Center(
+          child: Text('Página no encontrada'),
+        );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_getTitle(_selectedIndex)),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: () {
+              Scaffold.of(context).openDrawer();
+            },
+          ),
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            ListTile(
+              title: Text('Cliente'),
+              selected: _selectedIndex == 0,
+              onTap: () {
+                _onItemTapped(0);
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Empleados'),
+              selected: _selectedIndex == 1,
+              onTap: () {
+                _onItemTapped(1);
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Productos'),
+              selected: _selectedIndex == 2,
+              onTap: () {
+                _onItemTapped(2);
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Provedores'),
+              selected: _selectedIndex == 3,
+              onTap: () {
+                _onItemTapped(3);
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Ventas'),
+              selected: _selectedIndex == 4,
+              onTap: () {
+                _onItemTapped(4);
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+      body: _getPageBody(_selectedIndex),
+    );
+  }
+}
